ce2168e5ff8e3b35e923a549d4230f0fe0931e93
Stephen Carlin <scarlinj@gmail.com>
2022-02-21 21:50:08 -0800

    fixed function to ask question and store object values

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,6 @@
 // TODO: Include packages needed for this application
+var inq = require("inquirer")
+var genMark = require("./utils/generateMarkdown.js")
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -7,7 +9,22 @@ const questions = [];
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inq.prompt({
+            type: 'input',
+            name: 'title',
+            message: 'What do you want to call this app?',
+
+        })
+        .then((answers) => {
+            var X = genMark(answers)
+            console.log(X);
+            console.log(answers);
+        })
+        .catch((banana) => {
+            console.log(banana);
+        });
+}
 
 // Function call to initialize app
 init();
